2c47dc19ef7646844161c8d35774cdafdb1c9bc9
Marco van den Oever <marco@flyingpie.nl>
2024-06-05 01:29:51 +0200

    [Wezterm] Background and font size

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,3 +1,4 @@
+-- Pull in the wezterm API.
 local wezterm = require("wezterm")
 
 local config = {}
@@ -6,5 +7,9 @@ config.color_scheme = "Neon Night (Gogh)"
 config.color_scheme = "iTerm2 Smoooooth"
 
 config.font = wezterm.font("CaskaydiaMono Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
+config.font_size = 10.0
+
+config.window_background_image = wezterm.home_dir .. "/.home/img/bg.png"
+config.window_background_image_hsb = { brightness = 0.01 }
 
 return config
