a3365de710adc45479e30ae19bbeca9087c0ce29
Marco van den Oever <marco@flyingpie.nl>
2024-06-16 19:40:52 +0200

    [WezTerm] Color scheme update

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,13 +3,14 @@ local wezterm = require("wezterm")
 
 local config = {}
 
-config.color_scheme = "Neon Night (Gogh)"
-config.color_scheme = "iTerm2 Smoooooth"
-config.color_scheme = "City Lights"
+-- Color scheme
+config.color_scheme = "Rapture"
 
+-- Font
 config.font = wezterm.font("CaskaydiaMono Nerd Font", {weight="Regular", stretch="Normal", style="Normal"})
 config.font_size = 10.0
 
+-- Background
 config.window_background_image = wezterm.home_dir .. "/.home/img/bg.png"
 config.window_background_image_hsb = { brightness = 0.004 }
 
