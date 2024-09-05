d45b397ed116a64232650a672ff34fbc38a78d6c
Marco van den Oever <marco@flyingpie.nl>
2024-09-05 23:44:48 +0200

    [NVim] Some Neotree tweaks

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -3,18 +3,19 @@ return {
 	opts = {
 		filesystem = {
 			filtered_items = {
-	 visible = true,
-	 show_hidden_count = true,
-	 hide_dotfiles = false,
-	 hide_gitignored = true,
-	 hide_by_name = {
-	   -- '.git',
-	   -- '.DS_Store',
-	   -- 'thumbs.db',
-	 },
-	never_show = {},
-      },
-    }
-  }
+				visible = true,
+				hide_dotfiles = false,
+				hide_gitignored = true,
+				hide_by_name = {
+					-- '.git',
+					-- '.DS_Store',
+					-- 'thumbs.db',
+				},
+				never_show = {},
+				show_hidden_count = true,
+				use_libuv_file_watcher = false,
+			}
+		}
+	}
 }
 
