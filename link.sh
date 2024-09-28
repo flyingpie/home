de48c4a372920d39966e150312b2f129cae8f303
Marco van den Oever <marco@flyingpie.nl>
2024-09-28 14:56:25 +0200

    [GitConfig] v1

--- link.sh
+++ link.sh
@@ -7,6 +7,9 @@ ln -s $(pwd)/bin ~/.local/bin
 mkdir -p ~/.local/share
 ln -s $(pwd)/fonts ~/.local/share/fonts
 
+# Git
+ln -srf $(pwd)/.gitconfig ~/.gitconfig
+
 # SSH Agent
 mkdir -p ~/.config/systemd/user
 ln -s $(pwd)/ssh/ssh-agent.service ~/.config/systemd/user/ssh-agent.service
