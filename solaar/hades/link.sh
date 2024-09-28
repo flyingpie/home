427096b631a599f83a10f4c926ca8316e6add5aa
Marco van den Oever <marco@flyingpie.nl>
2024-09-28 15:51:18 +0200

    [Solaar] UDev rules

--- solaar/hades/link.sh
+++ solaar/hades/link.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-ln -srf $(pwd) ~/.config/solaar
+# Native
+rm -rf ~/.config/solaar
+ln -srT $(pwd) ~/.config/solaar
+ln -sf /usr/share/applications/solaar.desktop ~/.config/autostart
 
+# Flatpak
+#rm -rf ~/.var/app/io.github.pwr_solaar.solaar/config/solaar
+#ln -srT $(pwd) ~/.var/app/io.github.pwr_solaar.solaar/config/solaar
+
+sudo ln -srf $(pwd)/udev.rules /etc/udev/rules.d/42-logitech-unify-permissions.rules
+sudo chown $USER /dev/uinput
+sudo udevadm control --reload-rules
