e33ad2f2ce6d3e8e0ca21e9617ec150de65eef91
Marco van den Oever <marco@flyingpie.nl>
2024-12-08 23:37:47 +0100

    [Arch] install-arch.sh & install-yay.sh

---
+++ install-arch.sh
@@ -0,0 +1,17 @@
+# Native Packages
+sudo pacman -S \
+	firefox \
+	flatpak \
+	htop \
+	keepassxc \
+	krusader \
+	ncdu \
+	qt6-imageformats \
+	rclone \
+	solaar \
+	strace \
+	tailscale \
+	tmux \
+	wezterm \
+	wl-clipboard \
+	xdg-desktop-portal-gtk
---
+++ install-yay.sh
@@ -0,0 +1,6 @@
+# Yay
+
+## Install
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
