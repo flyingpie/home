2a20beac8df7260b89c4aaf181135a474d1e1959
Marco van den Oever <marco@flyingpie.nl>
2024-12-22 14:48:53 +0100

    [NVim] Roslyn"

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -1,20 +1,27 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(vim.env.LAZY or lazypath)
 
 require("lazy").setup({
 	checker = {
-		enabled = false,	-- automatically check for plugin updates
-		notify = true,		-- get a notification when new updates are found
+		enabled = false, -- automatically check for plugin updates
+		notify = true, -- get a notification when new updates are found
 	},
 	defaults = {
 		-- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
 		-- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-		lazy = true,
+		-- lazy = true,
 		-- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
 		-- have outdated releases, which may break your Neovim install.
 		version = false, -- always use the latest git commit
@@ -44,7 +51,7 @@ require("lazy").setup({
 		-- import any extras modules here
 		-- { import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
-		{ import = "lazyvim.plugins.extras.lang.omnisharp" },
+		-- { import = "lazyvim.plugins.extras.lang.omnisharp" },
 		-- { import = "lazyvim.plugins.extras.ui.mini-animate" },
 		-- import/override with your plugins
 		{ import = "plugins" },
