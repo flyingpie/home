ba4da6a104c0337e26bfcd96d2c9732d54d0b436
Marco van den Oever <marco@flyingpie.nl>
2025-06-14 21:47:05 +0200

    Workspace

--- init.ps1
+++ init.ps1
@@ -1,1 +1,10 @@
-# TODO
+
+$ErrorActionPreference = "Stop"
+
+Write-Host "Pulling submodules" -f cyan
+git submodule update --init
+
+Write-Host "Linking settings files" -f cyan
+./scripts/link.ps1
+
+Write-Host "Done!" -f green
--- scripts/install.ps1
+++ scripts/install.ps1
@@ -1,51 +1,65 @@
+param (
+	[switch] $Full
+)
+
 # Scoop
+# TODO: Check if installed
 #Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 #Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
+# Buckets
 scoop bucket add extras
 scoop bucket add nerd-fonts
 scoop bucket add sysinternals
 scoop bucket add versions
 
+# Apps
+scoop install main/7zip
+scoop install main/docker
 scoop install main/eza
 scoop install main/fd
+scoop install main/fzf
+scoop install main/gzip
+scoop install main/git
 scoop install main/lazygit
+scoop install main/lua
+scoop install main/luarocks
+scoop install main/maven
 scoop install main/mingw
 scoop install main/neovim
 scoop install main/nodejs
+scoop install main/nuget
+scoop install main/pwsh
 scoop install main/python
+scoop install main/ripgrep
+scoop install main/unzip
+scoop install main/wget
 
-scoop install extras/jetbrains-toolbox
+scoop install extras/doublecmd
 scoop install extras/keepassxc
 scoop install extras/klogg
+scoop install extras/notepadplusplus
 scoop install extras/q-dir
-scoop install extras/spotify
-#scoop install extras/tailscale # Doesn't work properly yet, regular install for now
+scoop install extras/sharex
 scoop install extras/wezterm
-scoop install extras/wireshark
+scoop install extras/windows-terminal
 
 scoop install nerd-fonts/CascadiaMono-NF
 
 scoop install sysinternals/process-explorer
 
-#7zip
-#bruno
-#chromium
-#docker
-#doublecmd
-#git
-#klogg
-#maven
-#neovide
-#notepadplusplus
-#nuget
-#processhacker
-#pwsh
-#rustdesk
-#sharex
-#sunshine
-#synctrayzor
-#temurin11-jdk
-#vlc
-#vscode
-#wireshark
+scoop install versions/dotnet-sdk-lts
+scoop install versions/systeminformer-nightly
+scoop install versions/temurin11-jdk
+
+# Extras
+if ($Full) {
+	scoop install extras/bruno
+	scoop install extras/chromium
+	scoop install extras/jetbrains-toolbox
+	scoop install extras/spotify
+	scoop install extras/synctrayzor
+	#scoop install extras/tailscale # Doesn't work properly yet, regular install for now
+	scoop install extras/vscode
+	scoop install extras/wireshark
+}
