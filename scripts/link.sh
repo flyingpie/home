f1617ddbc733bde9b4d21e839b192fff29c0f766
Marco van den Oever <marco@flyingpie.nl>
2025-06-21 16:17:18 +0200

    [Link] Fixed relative paths

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,103 +1,105 @@
 #/bin/bash
 
+H=$(pwd)/..
+
 # Bin
 rm -rf ~/.local/bin
-ln -s $(pwd)/bin ~/.local/bin
+ln -s $H/bin ~/.local/bin
 
 # Dolphin
-ln -srf $(pwd)/dolphin/dolphinrc ~/.config/dolphinrc
+ln -srf $H/dolphin/dolphinrc ~/.config/dolphinrc
 
 # Fonts
 rm -rf ~/.local/share/fonts
 mkdir -p ~/.local/share
-ln -srf $(pwd)/fonts ~/.local/share/fonts
+ln -srf $H/fonts ~/.local/share/fonts
 
 # Git
-# ln -srf $(pwd)/.gitconfig ~/.gitconfig
+# ln -srf $H/.gitconfig ~/.gitconfig
 
 # Ghostty
 rm -rf ~/.config/ghostty
-ln -srf $(pwd)/ghostty ~/.config/ghostty
+ln -srf $H/ghostty ~/.config/ghostty
 
 # JetBrains - DataGrip
 mkdir -p ~/.config/JetBrains/DataGrip2025.1
-ln -srf $(pwd)/datagrip/idea.properties ~/.config/JetBrains/DataGrip2025.1/idea.properties
+ln -srf $H/datagrip/idea.properties ~/.config/JetBrains/DataGrip2025.1/idea.properties
 
 # JetBrains - Rider
-ln -srf $(pwd)/rider/idea.properties ~/.config/JetBrains/Rider2024.2/idea.properties
-ln -srf $(pwd)/rider/idea.properties ~/.config/JetBrains/Rider2024.3/idea.properties
-ln -srf $(pwd)/rider/idea.properties ~/.config/JetBrains/Rider2025.1/idea.properties
+ln -srf $H/rider/idea.properties ~/.config/JetBrains/Rider2024.2/idea.properties
+ln -srf $H/rider/idea.properties ~/.config/JetBrains/Rider2024.3/idea.properties
+ln -srf $H/rider/idea.properties ~/.config/JetBrains/Rider2025.1/idea.properties
 
 # Kando
 mkdir -p ~/.config/kando
-ln -srf $(pwd)/kando/config.json	~/.config/kando/config.json
-ln -srf $(pwd)/kando/menus.json		~/.config/kando/menus.json
+ln -srf $H/kando/config.json	~/.config/kando/config.json
+ln -srf $H/kando/menus.json		~/.config/kando/menus.json
 mkdir -p ~/.var/app/menu.kando.Kando/config/kando
-ln -srf $(pwd)/kando/config.json	~/.var/app/menu.kando.Kando/config/kando/config.json
-ln -srf $(pwd)/kando/menus.json		~/.var/app/menu.kando.Kando/config/kando/menus.json
+ln -srf $H/kando/config.json	~/.var/app/menu.kando.Kando/config/kando/config.json
+ln -srf $H/kando/menus.json		~/.var/app/menu.kando.Kando/config/kando/menus.json
 
 # LazyGit
 mkdir -p ~/.config/lazygit
-ln -srf $(pwd)/lazygit/config.yml ~/.config/lazygit/config.yml
+ln -srf $H/lazygit/config.yml ~/.config/lazygit/config.yml
 
 # NVim
 rm -rf ~/.config/nvim
-ln -srf $(pwd)/nvim ~/.config/nvim
+ln -srf $H/nvim ~/.config/nvim
 mkdir -p ~/.var/app/io.neovim.nvim/config/nvim
-ln -srf $(pwd)/nvim ~/.var/app/io.neovim.nvim/config/nvim
+ln -srf $H/nvim ~/.var/app/io.neovim.nvim/config/nvim
 
 # PowerShell
 rm -rf ~/.config/powershell
-ln -srf $(pwd)/ps ~/.config/powershell
+ln -srf $H/ps ~/.config/powershell
 
 # Rofi
 rm -rf ~/.config/rofi
-ln -srf $(pwd)/rofi ~/.config/rofi
+ln -srf $H/rofi ~/.config/rofi
 
 # Solaar
 rm -rf ~/.config/solaar
-ln -srT $(pwd)/solaar ~/.config/solaar
+ln -srT $H/solaar ~/.config/solaar
 ln -sf /usr/share/applications/solaar.desktop ~/.config/autostart
 
-sudo cp -f $(pwd)/solaar/udev.rules /etc/udev/rules.d/42-logitech-unify-permissions.rules
+sudo cp -f $H/solaar/udev.rules /etc/udev/rules.d/42-logitech-unify-permissions.rules
 sudo chown $USER /dev/uinput
 sudo udevadm control --reload-rules
 
 # SSH Agent
 mkdir -p ~/.config/systemd/user
-ln -srf $(pwd)/ssh/ssh-agent.service ~/.config/systemd/user/ssh-agent.service
+ln -srf $H/ssh/ssh-agent.service ~/.config/systemd/user/ssh-agent.service
 
 # TMux
-ln -srf $(pwd)/tmux/.tmux.conf ~/.tmux.conf
+ln -srf $H/tmux/.tmux.conf ~/.tmux.conf
 
 # User
-ln -srf $(pwd)/user/.profile ~/.bash_profile
-ln -srf $(pwd)/user/.profile ~/.profile
-ln -srf $(pwd)/user/.profile ~/.zshenv
+ln -srf $H/user/.profile ~/.bash_profile
+ln -srf $H/user/.profile ~/.profile
+ln -srf $H/user/.profile ~/.zshenv
 
 # VSCode
 rm -rf  ~/.config/Code/User/
 mkdir -p ~/.config/Code/User/
-ln -s $(pwd)/vscode/keybindings.json ~/.config/Code/User/keybindings.json
-ln -s $(pwd)/vscode/settings.json ~/.config/Code/User/settings.json
+ln -s $H/vscode/keybindings.json ~/.config/Code/User/keybindings.json
+ln -s $H/vscode/settings.json ~/.config/Code/User/settings.json
 
 # Wezterm
 mkdir -p ~/.config/wezterm
-ln -srf $(pwd)/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
+ln -srf $H/wezterm/wezterm.lua ~/.config/wezterm/wezterm.lua
 
 # WTQ
 mkdir -p ~/.config/wtq
-ln -srf $(pwd)/wtq/wtq.jsonc ~/.config/wtq/wtq.jsonc
+ln -srf $H/wtq/wtq.jsonc ~/.config/wtq/wtq.jsonc
 
 # Zed
 rm -rf ~/.config/zed
-ln -srf $(pwd)/zed ~/.config/zed
+ln -srf $H/zed ~/.config/zed
 
 # Zellij
 mkdir -p ~/.config/zellij
-ln -srf $(pwd)/zellij/config.kdl ~/.config/zellij/config.kdl
+ln -srf $H/zellij/config.kdl ~/.config/zellij/config.kdl
 
 # Zsh
-ln -srf $(pwd)/zsh/.zshrc ~/.zshrc
+ln -srf $H/zsh/.zshrc ~/.zshrc
 
 #fc-cache -fv
