89c10d29082b013994d48137ee2e2c68517961a4
Marco van den Oever <marco@flyingpie.nl>
2025-09-06 15:16:55 +0200

    Workspace

--- kde/link.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ln -srf $(pwd)/kglobalshortcutsrc ~/.config/kglobalshortcutsrc
-
--- krusader/link.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-ln -srf $(pwd)/krusaderrc ~/.config/krusaderrc
-
-mkdir -p ~/.local/share/kxmlgui5/krusader
-ln -srf $(pwd)/krusaderui.rc ~/.local/share/kxmlgui5/krusader/krusaderui.rc
