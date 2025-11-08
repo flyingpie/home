d019858cfd9971eb345150ec8c5d295cd85e6dbc
Marco van den Oever <marco.vandenoever@datarotonde.com>
2025-11-08 23:25:33 +0100

    [NVim] Updated plugins

--- nvim-v2/lua/plugins/editor.lua
+++ nvim-v2/lua/plugins/editor.lua
@@ -3,12 +3,6 @@ return {
 	-- bufferline
 	{
 		"akinsho/bufferline.nvim",
-		init = function()
-			local bufline = require("catppuccin.groups.integrations.bufferline")
-			function bufline.get()
-				return bufline.get_theme()
-			end
-		end,
 	},
 
 	-- minimap
