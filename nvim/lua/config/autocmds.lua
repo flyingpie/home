82358422c2fdde98e5fae80af8f6240b05a3b060
Marco van den Oever <marco@flyingpie.nl>
2026-02-28 16:49:53 +0100

    [NVim] Removed v1

--- nvim/lua/config/autocmds.lua
+++
@@ -1,9 +0,0 @@
--- Use "jsonc" filetype for "json" files, so we can use comments and such.
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = "*.json",
-	callback = function()
-		vim.bo.filetype = "jsonc"
-	end,
-})
-
-vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
