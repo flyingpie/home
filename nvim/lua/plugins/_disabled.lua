0c591c7c131956032ea86f35f9bbd43793661573
Marco van den Oever <marco@flyingpie.nl>
2026-03-12 22:28:24 +0100

    [NVim] Snippets

--- nvim/lua/plugins/_disabled.lua
+++ nvim/lua/plugins/_disabled.lua
@@ -1,4 +1,5 @@
 return {
 	{ "folke/noice.nvim", enabled = false },
 	{ "nvim-mini/mini.pairs", enabled = false },
+	{ "rafamadriz/friendly-snippets", enabled = false },
 }
--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -88,27 +88,31 @@ return {
 		end,
 	},
 
+	-- nvim-treesitter-context
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		opts = {
+			enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+			multiwindow = true, -- Enable multiwindow support.
+			max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+			min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+			line_numbers = true,
+			multiline_threshold = 20, -- Maximum number of lines to show for a single context
+			trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+			mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+			-- Separator between context and content. Should be a single character string, like '-'.
+			-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+			separator = nil, -- "┈",
+			zindex = 20, -- The Z-index of the context window
+			on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+		},
+	},
+
 	-- outline
 	{
 		"hedyhli/outline.nvim",
 		version = "*",
 		opts = {
-			outline_window = {
-				position = "right",
-				show_numbers = true,
-				show_relative_numbers = true,
-				split_command = nil,
-				width = 30,
-			},
-
-			symbol_folding = {
-				autofold_depth = false,
-			},
-
-			preview_window = {
-				auto_preview = false,
-			},
-
 			-- stylua: ignore
 			keymaps = {
 				close					= "<Esc>",
@@ -130,6 +134,22 @@ return {
 				unfold					= "l",
 				unfold_all				= "E",
 				up_and_jump				= "<C-k>",
+			},
+
+			outline_window = {
+				position = "right",
+				show_numbers = true,
+				show_relative_numbers = true,
+				split_command = nil,
+				width = 30,
+			},
+
+			preview_window = {
+				auto_preview = false,
+			},
+
+			symbol_folding = {
+				autofold_depth = false,
 			},
 		},
 	},
