7ac2e8bb718066159558469ac0826a7f68c56cd5
Marco van den Oever <marco@flyingpie.nl>
2026-06-02 21:42:43 +0200

    feat(nvim): go

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.clangd" },
 		{ import = "lazyvim.plugins.extras.lang.docker" },
 		{ import = "lazyvim.plugins.extras.lang.git" },
+		{ import = "lazyvim.plugins.extras.lang.go" },
 		{ import = "lazyvim.plugins.extras.lang.helm" },
 		{ import = "lazyvim.plugins.extras.lang.java" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
