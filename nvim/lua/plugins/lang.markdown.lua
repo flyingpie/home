4d3c6b3ea4d4c89ea7bfd2612db2bf4605a40006
Marco van den Oever <marco@flyingpie.nl>
2026-08-10 12:10:27 +0200

    neovim: render-markdown.nvim

--- nvim/lua/plugins/lang.markdown.lua
+++ nvim/lua/plugins/lang.markdown.lua
@@ -1,17 +1,14 @@
 return {
 
-	-- touchup
-	{ "noisesfromspace/touchup.nvim", opts = {} },
-
-	-- -- markdown
-	-- {
-	-- 	"MeanderingProgrammer/render-markdown.nvim",
-	-- 	version = "*",
-	-- 	opts = {
-	-- 		code = { -- Makes code blocks stay put when moving the cursor over them, and when switching modes.
-	-- 			border = "none",
-	-- 			conceal_delimiters = false,
-	-- 		},
-	-- 	},
-	-- },
+	-- markdown
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		version = "*",
+		opts = {
+			code = { -- Makes code blocks stay put when moving the cursor over them, and when switching modes.
+				border = "none",
+				conceal_delimiters = false,
+			},
+		},
+	},
 }
