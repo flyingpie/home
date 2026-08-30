112a3df0ab105be659d530a3798f09a470a812c1
Marco van den Oever <marco@flyingpie.nl>
2026-08-30 18:46:26 +0200

    nvim: touchup -> markdown

--- nvim/lua/plugins/lang.markdown.lua
+++ nvim/lua/plugins/lang.markdown.lua
@@ -1,17 +1,17 @@
 return {
 
-	-- touchup
-	{ "noisesfromspace/touchup.nvim", opts = {} },
+	-- -- touchup
+	-- { "noisesfromspace/touchup.nvim", opts = {} },
 
-	-- -- markdown
-	-- {
-	-- 	"MeanderingProgrammer/render-markdown.nvim",
-	-- 	version = "*",
-	-- 	opts = {
-	-- 		code = { -- Makes code blocks stay put when moving the cursor over them, and when switching modes.
-	-- 			border = "none",
-	-- 			conceal_delimiters = false,
-	-- 		},
-	-- 	},
-	-- },
+	-- markdown
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		version = "*",
+		opts = {
+			code = { -- Makes code blocks stay put when moving the cursor over them, and when switching modes.
+				border = "none",
+				conceal_delimiters = false,
+			},
+		},
+	},
 }
